4402a8c92bb6fd17f103d0e61ae95c15105f3ad5
ArcNinjaC <121591277+ArcNinjaC@users.noreply.github.com>
2026-02-17 14:31:14 -0500

    programmed the phyiscs

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ NUM_GRIDS_Y = 400
 DELX = COORDINATE_SYSTEM[0]*2 / (NUM_GRIDS_X)
 DELY = COORDINATE_SYSTEM[1]*2 / (NUM_GRIDS_Y)
 
-FPS = 60
+FPS = 1000
 DT = 1/FPS
 
-xcoordinatesraw = np.linspace(-COORDINATE_SYSTEM[0], COORDINATE_SYSTEM[0], NUM_GRIDS_X,endpoint=False)
-ycoordinatesraw = np.linspace(-COORDINATE_SYSTEM[1],COORDINATE_SYSTEM[1],NUM_GRIDS_Y,endpoint=False)
+xcoordinatesraw = np.linspace(-COORDINATE_SYSTEM[0], COORDINATE_SYSTEM[0], NUM_GRIDS_X,endpoint=False,dtype=np.float32)
+ycoordinatesraw = np.linspace(-COORDINATE_SYSTEM[1],COORDINATE_SYSTEM[1],NUM_GRIDS_Y,endpoint=False,dtype=np.float32)
 xcoordinates, ycoordinates =  np.meshgrid(xcoordinatesraw,ycoordinatesraw)
 pressures = np.zeros((NUM_GRIDS_X,NUM_GRIDS_Y), dtype=np.float32)
 gradients = np.zeros((NUM_GRIDS_X,NUM_GRIDS_Y), dtype=np.float32)
-soundspeed = np.full((NUM_GRIDS_X,NUM_GRIDS_Y),343,dtype=np.float32)
+soundspeed = np.full((NUM_GRIDS_X,NUM_GRIDS_Y),1,dtype=np.float32)
 
 
 def perturb(intensity, position=(0,0), radius=0.1):
@@ -37,6 +37,18 @@ def pressure_color():
 
     return image
 
+def update_gradients():
+    laplacianx = pressures[:,:-2] - 2*pressures[:, 1:-1] + pressures[:, 2:]
+    laplacianx /= DELX**2
+    laplaciany = pressures[:-2, :] - 2*pressures[1:-1, :] + pressures[2:, :]
+    laplaciany /= DELY**2
+    gradients[:,1:-1] += (laplacianx)*DT*(soundspeed[:,1:-1])**2
+    gradients[1:-1,:] += laplaciany * DT * soundspeed[1:-1,:]**2
+
+def update_pressures():
+    global pressures
+    pressures += gradients * DT
+
 def main():
     pg.init()
     clock = pg.time.Clock()
@@ -45,10 +57,14 @@ def main():
     window = pg.display.set_mode(window_dimensions)
 
     perturb(1)
+    
 
     running = True
     while running:
+        
         surf_sound = pg.surfarray.make_surface(pressure_color())
+        update_gradients()
+        update_pressures()
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
